0e44d0f88647950c6aec0641b1c6931b3781e299
Harsh <98403303+SoniHarsh1@users.noreply.github.com>
2023-05-25 15:15:21 +0530

    adds banner

--- src/App.js
+++ src/App.js
@@ -1,15 +1,17 @@
 import "./App.css";
+// import Banner from "./pages/Banner";
 // import Team from "./pages/Team";
 // import Footer from "./pages/Footer";
 // import Blogs from "./pages/Blogs";
 
-
+// import BannerData from "./data/Banner.json";
 // import BlogsData from './data/Blogs.json';
 // import FooterData from "./data/Footer.json";
 
 function App() {
   return (
     <div className="App">
+      {/* <Banner {...BannerData}/> */}
       {/* <Team /> */}
       {/* <Blogs/> */}
       {/* <Footer {...FooterData}/> */}
---
+++ src/pages/Banner.jsx
@@ -0,0 +1,64 @@
+import React from "react";
+import { FaFacebook, FaTwitter, FaInstagram, FaLinkedin } from "react-icons/fa";
+import "./Banner.css";
+
+export default function Banner(props) {
+  return (
+    <div className="banner bg-white w-full h-screen">
+
+      {/* Background Video */}
+
+      <video
+        className="w-full h-full object-cover absolute z-10 opacity-20"
+        autoPlay
+        muted
+        loop
+        src={process.env.PUBLIC_URL + "/videos/banner_background.mp4"}
+      />
+
+      {/* Banner Text Block */}
+
+      <div className="relative isolate px-6 pt-14 lg:px-8 z-20">
+        <div className="mx-auto max-w-2xl h-[60vh] py-32 sm:py-48 lg:py-56 overflow-hidden">
+          <div className="text-center">
+            <p className="bannerTagline font-['Nunito'] font-bold text-[35px] -tracking-[0.04em] leading-[30px] text-[#1B262C]">
+              Spreading the sprit of Entreprenuership
+            </p>
+            <h1 className="bannerHeading pt-4 font-['Inter'] font-bold text-[50px] -tracking-[0.04em] leading-[60px] text-black capitalize">
+              Entrepreneurship Cell
+              <br />
+              IIT <span className="text-[#0060A1]"> Indore</span>
+            </h1>
+          </div>
+        </div>
+      </div>
+
+      {/* Social Media Block */}
+
+      <div className="socialMedia hidden lg:block absolute right-0 bottom-28 mr-6 z-50">
+        <div className="flex flex-col space-y-3">
+          <a href={props.linkedin} target="_blank">
+            <div className="w-[54px] h-[54px] rounded-full bg-[#D9D9D9] hover:bg-[#0060A1] text-[#0060A1] hover:text-white flex items-center justify-center">
+              <FaLinkedin className="text-4xl" />
+            </div>
+          </a>
+          <a href={props.linkedin} target="_blank">
+            <div className="w-[54px] h-[54px] rounded-full bg-[#D9D9D9] hover:bg-[#0060A1] text-[#0060A1] hover:text-white flex items-center justify-center">
+              <FaLinkedin className="text-4xl" />
+            </div>
+          </a>
+          <a href={props.linkedin} target="_blank">
+            <div className="w-[54px] h-[54px] rounded-full bg-[#D9D9D9] hover:bg-[#0060A1] text-[#0060A1] hover:text-white flex items-center justify-center">
+              <FaLinkedin className="text-4xl" />
+            </div>
+          </a>
+          <a href={props.linkedin} target="_blank">
+            <div className="w-[54px] h-[54px] rounded-full bg-[#D9D9D9] hover:bg-[#0060A1] text-[#0060A1] hover:text-white flex items-center justify-center">
+              <FaLinkedin className="text-4xl" />
+            </div>
+          </a>
+        </div>
+      </div>
+    </div>
+  );
+}
