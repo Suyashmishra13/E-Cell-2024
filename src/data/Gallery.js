eb99b9024e8e4312dd76a923097f3488a6ba1fc1
Pratham <pratham71003@gmail.com>
2023-05-26 12:22:52 +0530

    Renamed the Gallery.json and added data

---
+++ src/data/Gallery.js
@@ -0,0 +1,40 @@
+const gallery_data= 
+    [
+    {   "id" :1,
+        "key" :1,
+        "heading" : "E Summit",
+        "image_url" : "https://www2.shutterstock.com/blog/wp-content/uploads/sites/5/2016/11/EarthAbound_2880x1800_BlogHeader_ImageOnly_2.jpg",
+        "slide_data" : "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Architecto, accusantium, aliquam, est sapiente optio eveniet expedita exercitationem nobis dolore facilis sit non inventore voluptate quibusdam explicabo minus obcaecati aspernatur ab voluptatibus commodi qui? Commodi, necessitatibus, ipsam, voluptate amet omnis animi error eveniet consequatur velit dolorum ex earum vitae aperiam perspiciatis dignissimos quasi culpa molestiae "
+    },
+    {   "id" :2,
+        "key" :2,
+        "heading" : "E Summit",
+        "image_url" : "https://th.bing.com/th/id/OIP.avxhtQqr5FoubGjq4sIXugHaEo?w=313&h=196&c=7&r=0&o=5&dpr=1.3&pid=1.7",
+        "slide_data" : "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Architecto, accusantium, aliquam, est sapiente optio eveniet expedita exercitationem nobis dolore facilis sit non inventore voluptate quibusdam explicabo minus obcaecati aspernatur ab voluptatibus commodi qui? Commodi, necessitatibus, ipsam, voluptate amet omnis animi error eveniet consequatur velit dolorum ex earum vitae aperiam perspiciatis dignissimos quasi culpa molestiae "
+    },{
+        "id" :3,
+        "key" :3,
+        "heading" : "E Summit",
+        "image_url" : "https://th.bing.com/th/id/OIP.HfjNw2oY8kmVbHROFs0uaAHaEo?w=314&h=196&c=7&r=0&o=5&dpr=1.3&pid=1.7",
+        "slide_data" : "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Architecto, accusantium, aliquam, est sapiente optio eveniet expedita exercitationem nobis dolore facilis sit non inventore voluptate quibusdam explicabo minus obcaecati aspernatur ab voluptatibus commodi qui? Commodi, necessitatibus, ipsam, voluptate amet omnis animi error eveniet consequatur velit dolorum ex earum vitae aperiam perspiciatis dignissimos quasi culpa molestiae "
+    },{
+        "id" :4,
+        "key" :4,
+        "heading" : "E Summit",
+        "image_url" : "https://th.bing.com/th/id/OIP.9oXUiO50ST5XhL8agaEsiAHaEo?w=274&h=180&c=7&r=0&o=5&dpr=1.3&pid=1.7",
+        "slide_data" : "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Architecto, accusantium, aliquam, est sapiente optio eveniet expedita exercitationem nobis dolore facilis sit non inventore voluptate quibusdam explicabo minus obcaecati aspernatur ab voluptatibus commodi qui? Commodi, necessitatibus, ipsam, voluptate amet omnis animi error eveniet consequatur velit dolorum ex earum vitae aperiam perspiciatis dignissimos quasi culpa molestiae "
+    },{
+        "id" :5,
+        "key" :5,
+        "heading" : "E Summit",
+        "image_url" : "https://th.bing.com/th/id/OIP.jrCZ-qh-HP1NlmihB4-XRgAAAA?w=306&h=136&c=7&r=0&o=5&dpr=1.3&pid=1.7",
+        "slide_data" : "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Architecto, accusantium, aliquam, est sapiente optio eveniet expedita exercitationem nobis dolore facilis sit non inventore voluptate quibusdam explicabo minus obcaecati aspernatur ab voluptatibus commodi qui? Commodi, necessitatibus, ipsam, voluptate amet omnis animi error eveniet consequatur velit dolorum ex earum vitae aperiam perspiciatis dignissimos quasi culpa molestiae "
+    },{
+        "id" :6,
+        "key" :6,
+        "heading" : "E Summit",
+        "image_url" : "https://th.bing.com/th/id/OIP.WSYUlsPddIyVHUJUQBKVBAHaEK?w=272&h=180&c=7&r=0&o=5&dpr=1.3&pid=1.7",
+        "slide_data" : "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Architecto, accusantium, aliquam, est sapiente optio eveniet expedita exercitationem nobis dolore facilis sit non inventore voluptate quibusdam explicabo minus obcaecati aspernatur ab voluptatibus commodi qui? Commodi, necessitatibus, ipsam, voluptate amet omnis animi error eveniet consequatur velit dolorum ex earum vitae aperiam perspiciatis dignissimos quasi culpa molestiae "
+    }
+]
+export default gallery_data;
