11eddc7a73c923707215838e5bd4ea4e964d4254
Shuffled72 <shuffled720@gmail.com>
2023-06-01 22:59:27 +0530

    adds teams animations

--- src/pages/Team.jsx
+++ src/pages/Team.jsx
@@ -6,67 +6,95 @@ import PeopleCard from '../components/Team/PeopleCard'
 const Team = () => {
     return (
         <>
-            <div className='container mx-auto px-5'>
+            <div className='container mx-auto px-5 m-auto'>
 
                 <div className={TeamCss.header}>
                     <h1><span className={TeamCss.text_blue}>Our</span><span className={TeamCss.text_white}> Team</span></h1>
                 </div>
                 <section>
 
-                    <div className='flex felx-col  justify-between py-5   '>
+                    <div className='grid  grid-cols-3 md:grid-cols-5 gap-10 py-5 ' >
+                        <div></div>
+
                         <PeopleCard />
                         <PeopleCard />
                         <PeopleCard />
+                        <div></div>
+
                     </div>
                 </section>
                 <section>
 
-                    <div className=' flex m-auto w-1/3'>
+                    <div className=' grid grid-cols-1 md:grid-cols-3 gap-10 py-5'>
+                        <div></div>
                         <PeopleCard />
+                        <div></div>
                     </div>
-                    <div className=' flex m-auto w-1/3'>
+                    <div className=' grid grid-cols-1 md:grid-cols-3 gap-10 py-5'>
+                        <div></div>
                         <PeopleCard />
+                        <div></div>
                     </div>
                 </section>
                 <section>
 
-                    <div className='flex flex-auto justify-between py-5 '>
+                    <div className=' grid grid-cols-2 md:grid-cols-6 gap-12 py-5'>
+                        <div></div>
+
                         <PeopleCard />
                         <PeopleCard />
                         <PeopleCard />
                         <PeopleCard />
+                        <div></div>
+
                     </div>
 
                     <h3 className='text-white text-center font-bold pt-10'>Marketing Team</h3>
-                    <div className='flex flex-auto justify-between py-5 '>
+                    <div className=' grid grid-cols-2 md:grid-cols-6 gap-12 py-5'>
+                        <div></div>
+
                         <PeopleCard />
                         <PeopleCard />
                         <PeopleCard />
                         <PeopleCard />
+                        <div></div>
+
                     </div>
                     <h3 className='text-white text-center font-bold pt-10'>Social Media Team</h3>
 
-                    <div className='flex flex-auto justify-between py-5 '>
+                    <div className=' grid grid-cols-2 md:grid-cols-6 gap-12 py-5'>
+                        <div></div>
+
                         <PeopleCard />
                         <PeopleCard />
                         <PeopleCard />
                         <PeopleCard />
+                        <div></div>
+
                     </div>
                     <h3 className='text-white text-center font-bold pt-10'>Content Team</h3>
 
-                    <div className='flex flex-auto justify-between py-5 '>
+                    <div className=' grid grid-cols-2 md:grid-cols-6 gap-12 py-5'>
+                        <div></div>
+
                         <PeopleCard />
                         <PeopleCard />
                         <PeopleCard />
                         <PeopleCard />
+                        <div></div>
+
                     </div>
                     <h3 className='text-white text-center font-bold pt-10'>Web Development Team</h3>
 
-                    <div className='flex flex-auto justify-between py-5 '>
+                    <div className=' grid grid-cols-2 md:grid-cols-6 gap-12 py-5'>
+                        <div></div>
+
                         <PeopleCard />
                         <PeopleCard />
                         <PeopleCard />
                         <PeopleCard />
+                        <div></div>
+
                     </div>
                 </section>
 
