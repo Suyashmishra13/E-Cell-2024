a6772d0a95be35c466c7afbdca7df0ce5be8b5bd
Pratham <pratham71003@gmail.com>
2023-06-07 18:10:15 +0530

    added the data

--- src/data/Gallery.js
+++ src/data/Gallery.js
@@ -3,38 +3,85 @@ const gallery_data=
     {   "id" :1,
         "key" :1,
         "heading" : "E Summit",
-        "image_url" : "https://www2.shutterstock.com/blog/wp-content/uploads/sites/5/2016/11/EarthAbound_2880x1800_BlogHeader_ImageOnly_2.jpg",
-        "slide_data" : "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Architecto, accusantium, aliquam, est sapiente optio eveniet expedita exercitationem nobis dolore facilis sit non inventore voluptate quibusdam explicabo minus obcaecati aspernatur ab voluptatibus commodi qui? Commodi, necessitatibus, ipsam, voluptate amet omnis animi error eveniet consequatur velit dolorum ex earum vitae aperiam perspiciatis dignissimos quasi culpa molestiae "
+        "image_url" : "http://ecell.iiti.ac.in/esum1-01.jpg",
+        "slide_data" : "E-Summit is the flagship event organized by the E-Cell,IIT Indore annually.It is one of the most awaited events hosted by the institute that witnesses massive participation from across the country",
+        "disp_data":<><p>E-Summit is the flagship event organized by the E-Cell,IIT Indore annually.It is one of the most awaited events hosted by the institute that witnesses massive participation from across the country.Through this event,we aim to ignite the entrepreneur inside students by bringing them together to compete in various competitions exploring all aspects of entrepreneurship.</p>
+            <p>E-Cell,IIT Indore proudly claims to have successfully hosted editions of E-Summit and we are more than excited to take it to the next level this year,making it an honest celebration of the new wave of entrepreneurship !</p>
+            <p>There will be a total of 10 events focusing on all aspects of entrepreneurship and skills required to be a successful entrepreneur.</p>
+            <li>7,00,000 Fest Reach</li>
+            <li>5000 registrations for Competitions</li>
+            <li>3000+ Attendees</li>
+            <li>200+ Colleges</li>
+            <li>10,000+ Footfall</li>
+            <li>5k+ Campus Strength</li>
+
+        </>
     },
     {   "id" :2,
         "key" :2,
-        "heading" : "E Summit",
-        "image_url" : "https://th.bing.com/th/id/OIP.avxhtQqr5FoubGjq4sIXugHaEo?w=313&h=196&c=7&r=0&o=5&dpr=1.3&pid=1.7",
-        "slide_data" : "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Architecto, accusantium, aliquam, est sapiente optio eveniet expedita exercitationem nobis dolore facilis sit non inventore voluptate quibusdam explicabo minus obcaecati aspernatur ab voluptatibus commodi qui? Commodi, necessitatibus, ipsam, voluptate amet omnis animi error eveniet consequatur velit dolorum ex earum vitae aperiam perspiciatis dignissimos quasi culpa molestiae "
+        "heading" : "Marketing Fiesta",
+        "image_url" : "http://ecell.iiti.ac.in/gl-01.jpg",
+        "slide_data" : "The primary purpose of this particular competition was to facilitate a case study that was specifically designed to delve into the intricacies of marketing.  ",
+        "disp_data": <><p>The primary purpose of this particular competition was to facilitate a case study that was specifically designed to delve into the intricacies of marketing. The main aim was to explore and gain valuable insights and knowledge in the realm of marketing, which serves as the lifeblood of any successful business. Participants were tasked with putting their skills in the art of buying and selling to the test, as this competition served as a platform to assess their proficiency in this essential aspect of business operations.        </p></>
     },{
         "id" :3,
         "key" :3,
-        "heading" : "E Summit",
-        "image_url" : "https://th.bing.com/th/id/OIP.HfjNw2oY8kmVbHROFs0uaAHaEo?w=314&h=196&c=7&r=0&o=5&dpr=1.3&pid=1.7",
-        "slide_data" : "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Architecto, accusantium, aliquam, est sapiente optio eveniet expedita exercitationem nobis dolore facilis sit non inventore voluptate quibusdam explicabo minus obcaecati aspernatur ab voluptatibus commodi qui? Commodi, necessitatibus, ipsam, voluptate amet omnis animi error eveniet consequatur velit dolorum ex earum vitae aperiam perspiciatis dignissimos quasi culpa molestiae "
+        "heading" : "IITM NEXUS",
+        "image_url" : "http://ecell.iiti.ac.in/ENGEN-01.jpg",
+        "slide_data" : "A Book launch event was held for the book  'The IITM Nexus ' co authored by Dr.Srikanth Sundararajan, IITM Alumni Shree Pandey and IITI student Shibani Shashi. ",
+        "disp_data": <><p>A Book launch event was held for the book  "The IITM Nexus " co authored by Dr.Srikanth Sundararajan, IITM Alumni Shree Pandey and IITI student Shibani Shashi, a book about  the journey of 16 IIT Madras graduates who turned into business tycoons.</p></>
     },{
         "id" :4,
         "key" :4,
-        "heading" : "E Summit",
-        "image_url" : "https://th.bing.com/th/id/OIP.9oXUiO50ST5XhL8agaEsiAHaEo?w=274&h=180&c=7&r=0&o=5&dpr=1.3&pid=1.7",
-        "slide_data" : "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Architecto, accusantium, aliquam, est sapiente optio eveniet expedita exercitationem nobis dolore facilis sit non inventore voluptate quibusdam explicabo minus obcaecati aspernatur ab voluptatibus commodi qui? Commodi, necessitatibus, ipsam, voluptate amet omnis animi error eveniet consequatur velit dolorum ex earum vitae aperiam perspiciatis dignissimos quasi culpa molestiae "
+        "heading" : "Talk Sessions",
+        "image_url" : "http://ecell.iiti.ac.in/gl-01.jpg",
+        "slide_data" : "Talk shows remain the highlight of the summit each year,attracting a huge audience of students,professors and more from across different institutions. ",
+        "disp_data": <><p>Talk shows remain the highlight of the summit each year,attracting a huge audience of students,professors and more from across different institutions.This year we aim to make the talk show grand and glorious than ever,and will be hosting some of the most celebrated entrepreneurs who are sure to leave you spellbound with their journeys and experiences.</p><p>In the past editions,we have hosted entrepreneurs like Aman Dhattarwal, Sandeep Jain,Mayank Pratap and many more who have built their businesses from scratch and have brought revolution in their respective fields.We are excited to host yet another power packed edition to the talk show that will leave you energized to the core !</p></>
     },{
         "id" :5,
         "key" :5,
-        "heading" : "E Summit",
-        "image_url" : "https://th.bing.com/th/id/OIP.jrCZ-qh-HP1NlmihB4-XRgAAAA?w=306&h=136&c=7&r=0&o=5&dpr=1.3&pid=1.7",
-        "slide_data" : "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Architecto, accusantium, aliquam, est sapiente optio eveniet expedita exercitationem nobis dolore facilis sit non inventore voluptate quibusdam explicabo minus obcaecati aspernatur ab voluptatibus commodi qui? Commodi, necessitatibus, ipsam, voluptate amet omnis animi error eveniet consequatur velit dolorum ex earum vitae aperiam perspiciatis dignissimos quasi culpa molestiae "
+        "heading" : "IPL Auction",
+        "image_url" : "http://ecell.iiti.ac.in/e21-01.jpg",
+        "slide_data" : "Yet another crowd puller,the IPL auction will have another edition this year.This nail biting bidding war is sure to leave the audience at the edge of their seats !  ",
+        "disp_data": <><p>Yet another crowd puller,the IPL auction will have another edition this year.This nail biting bidding war is sure to leave the audience at the edge of their seats ! A wide range of audience ranging from cricket lovers to some who simply love the tense and competitive environment to some who wish to learn how decisions are taken to run businesses,are seen having an enjoyable experience.        </p><p>The previous edition was a grand success and this year too,we will be hosting an auction that will simulate the real one,we guarantee! </p></>
     },{
         "id" :6,
         "key" :6,
-        "heading" : "E Summit",
-        "image_url" : "https://th.bing.com/th/id/OIP.WSYUlsPddIyVHUJUQBKVBAHaEK?w=272&h=180&c=7&r=0&o=5&dpr=1.3&pid=1.7",
-        "slide_data" : "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Architecto, accusantium, aliquam, est sapiente optio eveniet expedita exercitationem nobis dolore facilis sit non inventore voluptate quibusdam explicabo minus obcaecati aspernatur ab voluptatibus commodi qui? Commodi, necessitatibus, ipsam, voluptate amet omnis animi error eveniet consequatur velit dolorum ex earum vitae aperiam perspiciatis dignissimos quasi culpa molestiae "
+        "heading" : "Case Study Competetion",
+        "image_url" : "http://ecell.iiti.ac.in/esum1-01.jpg",
+        "slide_data" : "Thinking is the most crucial part of any endeavour. It becomes essential to structure and organise your thoughts to make them useful for problem-solving. ",
+        "disp_data": <><p>Thinking is the most crucial part of any endeavour. It becomes essential to structure and organise your thoughts to make them useful for problem-solving. Any entrepreneur can only lead to success if they have the capability and passion for solving real-life problems. This competition gives a chance to the participants to check their skills in solving real-life problems.
+        The past editions have seen the glory of these competitions and this year too we will be hosting a better and enhanced version of it.</p></>
+    },{
+        "id" :7,
+        "key" :7,
+        "heading" : "Hackathon",
+        "image_url" : "http://ecell.iiti.ac.in/gl-01.jpg",
+        "slide_data" : "Technology has become an essential part of today’s world. No business can scale toady without the support of technology.  ",
+        "disp_data": <><p>Technology has become an essential part of today’s world. No business can scale toady without the support of technology. Hence we organize hackathons each year to check the coding skills of the participants and their ability to apply their talent in the real life business problems. This year as well we plan to host one more exciting and mind boggling edition of it.         </p></>
+    },{
+        "id" :8,
+        "key" :8,
+        "heading" : "Stock Trading Competition",
+        "image_url" : "http://ecell.iiti.ac.in/ENGEN-01.jpg",
+        "slide_data" : "Participating in a stock trading competition can be a great way to test your trading skills and knowledge. ",
+        "disp_data": <><p>Participating in a stock trading competition can be a great way to test your trading skills and knowledge. It provides a platform for the participants to showcase their abilities and win exciting prizes. Moreover, this competition also offers an opportunity to test your understanding of the market and the methods with which the bears and bulls operate. It also enhances the strategy-building, analyzing and decision-making skills of the participants. Alike the past years, this year also, we will be hosting one more version of this simulated competition.        </p></>
+    },{
+        "id" :9,
+        "key" :9,
+        "heading" : "Pitching Competition",
+        "image_url" : "http://ecell.iiti.ac.in/gl-01.jpg",
+        "slide_data" : "Marketing has evolved into one of the most crucial pillars for building brands in the past few years. ",
+        "disp_data": <><p>Marketing has evolved into one of the most crucial pillars for building brands in the past few years.Disruptive marketing strategies have the potential to completely transform market dynamics.To help our budding entrepreneurs learn exactly that,we will be hosting pitching competitions,that will help students refine their marketing and persuasive skills in the form of a healthy and fun competition.We aim to make a subtle effort to help students learn the importance of belief and trust of an entrepreneur on his product.<br></br>
+        Having hosted many pitching competitions in the past editions,this year too,we will be hosting a fun and competitive event with products and pitches of all kinds !
+        </p></>
+    },{
+        "id" :10,
+        "key" :10,
+        "heading" : "UI/UX (Introduction)",
+        "image_url" : "http://ecell.iiti.ac.in/e21-01.jpg",
+        "slide_data" : "UI refers to the screens, buttons, toggles, icons, and other visual elements that you interact with when using a website, app, or other electronic device. ",
+        "disp_data": <><p>UI refers to the screens, buttons, toggles, icons, and other visual elements that you interact with when using a website, app, or other electronic device. UX refers to the entire interaction you have with a product, including how you feel about the interaction. While UI can certainly have an impact on UX, the two are distinct, as are the roles that designers play.</p><p>UI/UX design has come to be an important technical skill for startups and businesses.This has revolutionized the entrepreneurial world with founders now paying special attention to grab that technological edge over their competitors.This enables entrepreneurs to attract more prospects, increase conversions, sales, and overall profitability, and improve customer retention.It helps build Brand loyalty and enhance customers' satisfaction.Recall the first time you used the lenskart app.We are sure you were flummoxed at how your online buying experience could be that interactive and satisfactory ! Yes,UI/UX does just that!        </p></>
     }
 ]
 export default gallery_data;
--- src/pages/Gallery.jsx
+++ src/pages/Gallery.jsx
@@ -1,6 +1,6 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Swiper, SwiperSlide } from 'swiper/react';
-import {FaArrowLeft , FaArrowRight} from "react-icons/fa";
+import {FaArrowLeft , FaArrowRight ,FaWindowClose} from "react-icons/fa";
 // import "../App.css"
 import "../css/Gallery.css"
 import gallery_data from '../data/Gallery';
@@ -15,8 +15,16 @@ import { EffectCoverflow, Pagination ,Navigation ,Autoplay} from 'swiper';
 
 
 export default function Gallery() {
+  const[visible , setvisibility] = useState("box invis");
+  const[index ,setindex]=useState(0);
   const [width, setWidth] = React.useState(window.innerWidth);
-  const breakpoint = 800;
+  const breakpoint = 1050;
+  function display_info(){
+    setvisibility("box vis");
+  }
+  function hide_info(){
+    setvisibility("box invis");
+  }
   React.useEffect(() => {
     
     window.addEventListener("resize", () => setWidth(window.innerWidth));
@@ -24,9 +32,13 @@ export default function Gallery() {
   return (
 <>
 <div className='gallery_container'>
+<div className = {visible}>
+  <h3 className="mt-3 text-center text-4xl font-black disp_head pt-6 pb-4">{gallery_data[index].heading}</h3>
+  {gallery_data[index].disp_data}
+  <button className='disp_btn' onClick={hide_info}><FaWindowClose/></button>
+</div>
         <h1 className="p-16 mt-0 heading">GALLER<span className="">Y</span></h1>
     <div className='container'>
-
         <Swiper
         effect={'coverflow'}
         grabCursor={true}
@@ -40,8 +52,8 @@ export default function Gallery() {
             modifier :2.5,
         }}
         pagination={{ el: '.swiper-pagination', clickable: true }}
-        autoplay={{delay : 1000 ,
-          disableOnInteraction: false,
+        autoplay={{delay : 3000 ,
+          disableOnInteraction: true,
         }}
         navigation={{
           nextEl: '.swiper-button-next',
@@ -51,16 +63,17 @@ export default function Gallery() {
         modules={[EffectCoverflow, Pagination, Navigation ,Autoplay]}
         className='swiper_container'
         >
-          {gallery_data.map((info ,index)=>(
+          {gallery_data.map((info ,ind)=>(
             <SwiperSlide>
               {({ isActive }) => (
                 
                 isActive ?<><div className='info rounded-3xl'>
                     <h1 className="info_heading">{info.heading}</h1>
                     <p className='info_data'>{info.slide_data}</p>
-                    <a href='#'>Know More...</a>
+                    {setindex(ind)}
+                    <button onClick={display_info}>Know More...</button>
                   </div> 
-                  <img src={/*process.env.PUBLIC_URL +*/ info.image_url} alt="images" className='gallery_img active_slide w-full xl:h-[400px] lg:h-[350px] object-cover rounded-3xl ' /></> :  <img src={process.env.PUBLIC_URL + info.image_url} alt="images" className='gallery_img w-full xl:h-[400px] lg:h-[300px] object-cover rounded-3xl '/>
+                  <img src={/*process.env.PUBLIC_URL +*/ info.image_url} alt="images" className='gallery_img active_slide w-full xl:h-[400px] lg:h-[350px] object-cover rounded-3xl' /></> :  <img src={process.env.PUBLIC_URL + info.image_url} alt="images" className='gallery_img w-full xl:h-[400px] lg:h-[300px] object-cover rounded-3xl '/>
                 // <div className='info'>Current slide is {isActive ? 'active' : 'not active'}</div>
               )}
               
